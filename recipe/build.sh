507999dc24c494015d9a7c0f8b0d404f2f49f1de
ArishaMays1 <1arishamays1@gmail.com>
2025-05-15 10:53:15 -0400

    Fix osx-64 build permissions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,17 @@
-make esbuild 
-mkdir -p $PREFIX/bin 
+set -ex
+
+export GOARCH=amd64
+
+export GOPATH=${SRC_DIR}/gopath
+mkdir -p ${GOPATH}
+export GO111MODULE=auto
+export CGO_ENABLED=0
+
+make esbuild
+
+find ${GOPATH} -type d -exec chmod u+w {} \;
+find ${GOPATH} -type f -exec chmod u+w {} \;
+
+mkdir -p $PREFIX/bin
 cp esbuild $PREFIX/bin/esbuild
 
